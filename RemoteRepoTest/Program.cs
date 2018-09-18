b75b12b08c2a91a145f47a7f32ef6ceef879b9fb
LAPTOP-73BPNIN4\pranas <pranas.vaicius@gmail.com>
2018-09-18 21:40:51 +0300

    ivykdyta uzduotis nr 2

--- RemoteRepoTest/Program.cs
+++ RemoteRepoTest/Program.cs
@@ -10,23 +10,51 @@ namespace RemoteRepoTest
     {
         static void Main(string[] args)
         {
+            //exercise1();
+            exercise2();
+            
+
+
+        }
+        static void exercise1()
+        {
             Console.WriteLine("iveskite 1 skaiciu:");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("iveskite 2 skaicius:");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("iveskite 3 skaiciu:");
             int c = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("didziausias is ju yra: "+Max(a,b,c));            
+            Console.WriteLine("didziausias is ju yra: " + Max(a, b, c));
             Console.WriteLine("press any key...");
             Console.ReadKey();
         }
-        static int Max (int a,int b, int c)
+        static void exercise2()
         {
-            if (a>b && a>c)
+            Console.WriteLine("iveskite 1 skaiciu:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iveskite 2 skaicius:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iveskite 3 skaiciu:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (InRange(a,b,c))
+            {
+                Console.WriteLine("pirmas skaicius yra rezyje tarp sekanciu dvieju");
+            }
+            else
+            {
+                Console.WriteLine("pirmas skaicius nera rezyje tarp sekanciu dvieju");
+            }
+            Console.WriteLine("didziausias is ju yra: " + Max(a, b, c));
+            Console.WriteLine("press any key...");
+            Console.ReadKey();
+        }
+        static int Max(int a, int b, int c)
+        {
+            if (a > b && a > c)
             {
                 return a;
             }
-            else if (b>a && b>c)
+            else if (b > a && b > c)
             {
                 return b;
             }
@@ -34,8 +62,17 @@ namespace RemoteRepoTest
             {
                 return c;
             }
-
-
+        }
+        static bool InRange(int testnum, int min, int max)
+        {
+            if (testnum>min && testnum<max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
