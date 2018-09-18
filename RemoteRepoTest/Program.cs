05509a449b345fe6c28bc9495adb44280d7ab0de
LAPTOP-73BPNIN4\pranas <pranas.vaicius@gmail.com>
2018-09-18 21:19:09 +0300

    parasyta programa pirmai uzduociai

--- RemoteRepoTest/Program.cs
+++ RemoteRepoTest/Program.cs
@@ -10,7 +10,32 @@ namespace RemoteRepoTest
     {
         static void Main(string[] args)
         {
-            //pakeitimas
+            Console.WriteLine("iveskite 1 skaiciu:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iveskite 2 skaicius:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iveskite 3 skaiciu:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("didziausias is ju yra: "+Max(a,b,c));            
+            Console.WriteLine("press any key...");
+            Console.ReadKey();
+        }
+        static int Max (int a,int b, int c)
+        {
+            if (a>b && a>c)
+            {
+                return a;
+            }
+            else if (b>a && b>c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+
+
         }
     }
 }
