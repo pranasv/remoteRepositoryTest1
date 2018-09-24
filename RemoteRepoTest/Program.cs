867d5d7c7199a8183a3cb4f0130b43f9025ea9a1
LAPTOP-73BPNIN4\pranas <pranas.vaicius@gmail.com>
2018-09-24 19:12:56 +0300

    pirma uzduotis

--- RemoteRepoTest/Program.cs
+++ RemoteRepoTest/Program.cs
@@ -10,6 +10,29 @@ namespace RemoteRepoTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("skaicius 1:");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("skaicius 2:");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("skaicius 3:");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Didziausias skaicius yra "+max(sk1, sk2, sk3));
+            Console.ReadKey();
+        }
+        static int max(int sk1, int sk2, int sk3)
+        {
+            if (sk1 > sk2 && sk1 > sk3)
+            {
+                return sk1;
+            }
+            else if (sk2 > sk1 && sk2 > sk3)
+            {
+                return sk2;
+            }
+            else
+            {
+                return sk3;
+            }
         }
     }
 }
